7df56e1ea51c1ec74ca1f78d2c6af286331e32dc
INSPIRON\Apone <spencer.bennett@gmail.com>
2019-06-10 07:56:06 -0500

    Stubbing recipe list and recipe detail.

--- src/app/functional/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/functional/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+    recipes: Recipe[] = [
+        new Recipe('Test Recipe', 'This is a test...', 'https://images.media-allrecipes.com/userphotos/560x315/1081388.jpg')
+    ];
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
